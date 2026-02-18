eebd4aeea1732aa8729a30af58f2b2fe71483f7c
AV_404 <aadarshvarma14@gmail.com>
2026-02-18 21:10:10 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -24,7 +24,7 @@ cacheSolve <- function(x, ...) {
                 return(inv)
                 }
         data <- x$get()
-        inv <- solve(data,...)
+        inv <- solve(data, ...)
         x$setinverse(inv)
         inv
         }
